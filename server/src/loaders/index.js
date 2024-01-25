7e5efde00097adfae72afdb67c0b3eb0d0c8dde0
BEB 09 박상현 <psh03225@naver.com>
2024-01-26 00:46:40 +0900

    Add: config.json.sample

--- server/src/loaders/index.js
+++ server/src/loaders/index.js
@@ -1,9 +1,9 @@
 const expressLoader = require('./express');
-// const mongooseLoader = require('./mongoose');
+const mongooseLoader = require('./mongoose');
 
 module.exports = async (app) => {
-  //   await mongooseLoader();
-  //   console.log('DB loaded and connected!');
+  await mongooseLoader();
+  console.log('DB loaded and connected!');
   await expressLoader(app);
   console.log('Express loaded');
 };
