f069e03ae0213224b159fe522291b5213c55eeff
BEB 09 박상현 <psh03225@naver.com>
2024-01-29 19:50:46 +0900

    Add: Users, nft models

---
+++ server/src/models/Nft.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const nftSchema = new mongoose.Schema({
+  token_id: {
+    // 토큰 ID
+    type: Number,
+    required: true,
+  },
+  user_id: {
+    // 사용자 ID
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  name: {
+    //변동 가능
+    type: String,
+    required: true,
+  },
+  description: {
+    //변동 가능
+    type: String,
+    default: '',
+  },
+  token_uri: {
+    //변동 가능
+    type: String,
+    required: true,
+  },
+  created_at: {
+    // DNFT 정보 생성일
+    type: Date,
+    default: getKorDate,
+  },
+  updated_at: {
+    // DNFT 정보 수정일
+    type: Date,
+    default: getKorDate,
+  },
+});
+
+const NFT = mongoose.model('nft', nftSchema);
+module.exports = NFT;
