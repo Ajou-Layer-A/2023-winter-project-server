3cc5a1cdc45e8f07bbc2a7653c6672eaf3866939
BEB 09 박상현 <psh03225@naver.com>
2024-02-08 21:29:37 +0900

    Add: update post

--- server/src/routes/posts.js
+++ server/src/routes/posts.js
@@ -1,81 +1,113 @@
-const Router = require("express");
-const postsController = require("../services/client/postsController");
-const jwtUtil = require("../utils/jwtUtils");
-const isAuth = require("../api/middlewares/isAuth");
+const Router = require('express');
+const postsController = require('../services/client/postsController');
+const jwtUtil = require('../utils/jwtUtils');
+const isAuth = require('../api/middlewares/isAuth');
 const route = Router();
-const config = require("../config/index");
-const postModel = require("../models/Post");
+const config = require('../config/index');
+const postModel = require('../models/Post');
 module.exports = (app) => {
-    app.use("/posts", route);
-    route.get("/delete/:post_id", isAuth, async (req, res) => {
-        const post_id = req.params.post_id;
-        if (!post_id) {
-            return res.status(400).json({
-                success: false,
-                message: "필수 입력값이 없습니다.",
-            });
-        }
-        const postData = await postModel.findById(post_id);
-        if (!postData) {
-            return res.status(400).json({
-                success: false,
-                message: "존재하지 않는 게시글입니다.",
-            });
-        }
-        if (
-            postData.user_id.toString() === req.decoded.user_id ||
-            req.decoded.isAdmin
-        ) {
-            // 게시글 삭제
-            const deleteResult = await postsController.deletePost(post_id);
-            if (!deleteResult.success) {
-                return res.status(400).json({
-                    success: false,
-                    message: "게시글 삭제에 실패했습니다.",
-                });
-            }
-        } else {
-            return res.status(400).json({
-                success: false,
-                message: "게시글 작성자가 아닙니다.",
-            });
-        }
-        return res.status(200).json({
-            success: true,
-            message: "게시글을 삭제했습니다.",
+  app.use('/posts', route);
+  route.get('/delete/:post_id', isAuth, async (req, res) => {
+    const post_id = req.params.post_id;
+    if (!post_id) {
+      return res.status(400).json({
+        success: false,
+        message: '필수 입력값이 없습니다.',
+      });
+    }
+    const postData = await postModel.findById(post_id);
+    if (!postData) {
+      return res.status(400).json({
+        success: false,
+        message: '존재하지 않는 게시글입니다.',
+      });
+    }
+    if (
+      postData.user_id.toString() === req.decoded.user_id ||
+      req.decoded.isAdmin
+    ) {
+      // 게시글 삭제
+      const deleteResult = await postsController.deletePost(post_id);
+      if (!deleteResult.success) {
+        return res.status(400).json({
+          success: false,
+          message: '게시글 삭제에 실패했습니다.',
         });
+      }
+    } else {
+      return res.status(400).json({
+        success: false,
+        message: '게시글 작성자가 아닙니다.',
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      message: '게시글을 삭제했습니다.',
     });
+  });
 
-    /**
-     * 게시글 저장
-     * @route POST /posts/create
-     * @param {*} title
-     * @param {*} category
-     */
-    route.post("/create", isAuth, async (req, res) => {
-        const postData = req.body;
-        if (!postData.title || !postData.category) {
-            return res.status(400).json({
-                success: false,
-                message: "Please give me Post Info",
-            });
-        }
-        //save Post data
-        const user_id = req.decoded.user_id;
-        const savePostResult = await postsController.savePostData(
-            user_id,
-            postData
-        );
-        if (!savePostResult.success) {
-            return res.status(400).json({
-                success: false,
-                message: "Save ERR" + savePostResult.message,
-            });
-        } else {
-            return res.status(200).json({
-                success: true,
-                message: savePostResult,
-            });
-        }
-    });
+  /**
+   * 게시글 저장
+   * @route POST /posts/create
+   * @param {*} title
+   * @param {*} category
+   */
+  route.post('/create', isAuth, async (req, res) => {
+    const postData = req.body;
+    if (!postData.title || !postData.category) {
+      return res.status(400).json({
+        success: false,
+        message: 'Please give me Post Info',
+      });
+    }
+    //save Post data
+    const user_id = req.decoded.user_id;
+    const savePostResult = await postsController.savePostData(
+      user_id,
+      postData
+    );
+    if (!savePostResult.success) {
+      return res.status(400).json({
+        success: false,
+        message: 'Save ERR' + savePostResult.message,
+      });
+    } else {
+      return res.status(200).json({
+        success: true,
+        message: savePostResult,
+      });
+    }
+  });
+
+  /**
+   * 게시글 수정 or NFT 판매완료
+   * @route POST /posts/update
+   * @param {*} post_id
+   * @param {*} postData
+   */
+  route.post('/update', isAuth, async (req, res) => {
+    const postData = req.body;
+    if (!postData.post_id) {
+      return res.status(400).json({
+        success: false,
+        message: 'Please give me Post Id',
+      });
+    }
+    const user_id = req.decoded.user_id;
+    const updatePostResult = await postsController.updatePostData(
+      user_id,
+      postData
+    );
+    if (!updatePostResult.success) {
+      return res.status(400).json({
+        success: false,
+        message: 'Update ERR' + updatePostResult.message,
+      });
+    } else {
+      return res.status(200).json({
+        success: true,
+        message: updatePostResult,
+      });
+    }
+  });
 };
--- server/src/services/client/postsController.js
+++ server/src/services/client/postsController.js
@@ -1,5 +1,5 @@
-const postModel = require("../../models/Post");
-const config = require("../../config/index");
+const postModel = require('../../models/Post');
+const config = require('../../config/index');
 
 /**
  * post delete
@@ -7,17 +7,17 @@ const config = require("../../config/index");
  * @returns 성공여부
  */
 const deletePost = async (postId) => {
-    try {
-        const result = await postModel.deleteOne({ _id: postId });
-        if (!result) {
-            return { success: false };
-        } else {
-            return { success: true };
-        }
-    } catch (err) {
-        console.error("Error:" + err);
-        throw Error(err);
+  try {
+    const result = await postModel.deleteOne({ _id: postId });
+    if (!result) {
+      return { success: false };
+    } else {
+      return { success: true };
     }
+  } catch (err) {
+    console.error('Error:' + err);
+    throw Error(err);
+  }
 };
 
 /**
@@ -27,34 +27,68 @@ const deletePost = async (postId) => {
  * @returns 성공여부
  */
 const savePostData = async (id, postData) => {
-    try {
-        const savePostData = new postModel({
-            title: postData.title,
-            user_id: id,
-            category: postData.category,
-            content: postData.content,
-        });
+  try {
+    const savePostData = new postModel({
+      title: postData.title,
+      user_id: id,
+      category: postData.category,
+      content: postData.content,
+    });
 
-        const result = await savePostData.save();
-        if (!result) {
-            return {
-                success: false,
-                message: "Post 정보 저장에 실패했습니다.",
-            };
-        }
-        return {
-            success: true,
-            data: result,
-        };
-    } catch (err) {
-        console.error(err);
-        throw Error(err);
+    const result = await savePostData.save();
+    if (!result) {
+      return {
+        success: false,
+        message: 'Post 정보 저장에 실패했습니다.',
+      };
     }
+    return {
+      success: true,
+      data: result,
+    };
+  } catch (err) {
+    console.error(err);
+    throw Error(err);
+  }
+};
+
+/**
+ * post update(if nft is sold out, is_sell = true)
+ * @param post_id
+ * @param postData
+ * @returns 성공여부
+ */
+const updatePostData = async (id, postData) => {
+  try {
+    const result = await postModel.updateOne(
+      { _id: id },
+      {
+        title: postData.title,
+        category: postData.category,
+        content: postData.content,
+        is_sell: postData.is_sell,
+      }
+    );
+    if (!result) {
+      return {
+        success: false,
+        message: 'Post 정보 수정에 실패했습니다.',
+      };
+    }
+    return {
+      success: true,
+      data: result,
+    };
+  } catch (err) {
+    console.error(err);
+    throw Error(err);
+  }
 };
 
 module.exports = {
-    savePostData,
-    deletePost,
+  savePostData,
+  deletePost,
+  updatePostData,
 };
 
 // Refresh
